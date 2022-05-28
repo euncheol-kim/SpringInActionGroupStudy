ecb5e3e6d848b2fd06a3a0c8778e6ee4b8d7dc56
nicebyy <45655434+nicebyy@users.noreply.github.com>
2022-05-29 04:37:04 +0900

    chapter4 upload

--- nicebyy/taco/tacos/src/main/java/springinaction/tacos/web/WebConfig.java
+++ nicebyy/taco/tacos/src/main/java/springinaction/tacos/web/WebConfig.java
@@ -4,11 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
+//        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login");
     }
 }
