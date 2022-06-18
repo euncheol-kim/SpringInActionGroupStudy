db6bb3b2e321117cef0e25b1df91cb37e09aa33c
Euncheol Kim <72078208+euncheol-kim@users.noreply.github.com>
2022-06-19 08:35:31 +0900

    Add files via upload

---
+++ Euncheol_Kim/실습코드/taco-cloud(3장까지진행)/taco-cloud/src/main/java/tacos/Order.java
@@ -0,0 +1,64 @@
+package tacos;
+
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name="Taco_Order")
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Date placedAt;
+
+    @NotBlank(message="Name is required")
+    private String deliveryName;
+
+    @NotBlank(message="Street is required")
+    private String deliveryStreet;
+
+    @NotBlank(message="City is required")
+    private String deliveryCity;
+
+    @NotBlank(message="State is required")
+    private String deliveryState;
+
+    @NotBlank(message="Zip code is required")
+    private String deliveryZip;
+
+    @CreditCardNumber(message="Not a valid credit card number")
+    private String ccNumber;
+
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+            message="Must be formatted MM/YY")
+    private String ccExpiration;
+
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
+    private String ccCVV;
+
+    @ManyToMany
+    private List<Taco> tacos = new ArrayList<>();
+
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
+    }
+
+    public void addDesign(Taco design) {
+        this.tacos.add(design);
+    }
+}
---
+++ Euncheol_Kim/실습코드/taco-cloud(3장까지진행)/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -0,0 +1,80 @@
+package tacos.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import tacos.Ingredient;
+import tacos.Ingredient.Type;
+import tacos.Order;
+import tacos.Taco;
+import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Slf4j // 자바에서 사용하는 Simple Logging Facade Logger를 생성한다.
+@Controller // 스프링의 컨트롤러로 식별하게 하여, 스프링 컨텍스트 빈으로 DesignController의 인스턴스를 생성한다.
+@RequestMapping("/design")
+@SessionAttributes("order")
+public class DesignTacoController {
+
+    private final IngredientRepository ingredientRepo;
+    private TacoRepository tacoRepo;
+
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo) {
+        this.ingredientRepo = ingredientRepo;
+        this.tacoRepo = tacoRepo;
+    }
+
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors, @ModelAttribute Order order) {
+        Taco saved = tacoRepo.save(design);
+        order.addDesign(saved);
+
+        return "redirect:/orders/current";
+    }
+
+    @GetMapping
+    public String showDesignForm(Model model) { // Model -> Model은 컨트롤러와 데이터를 보여주는 뷰 사이에서 데이터를 운반하는 객체이다.
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepo.findAll().forEach(i->ingredients.add(i));
+
+        Type[] types = Ingredient.Type.values(); // enum Type의 상수를 배열에 저장한다.
+
+        for(Type type : types){
+            // Model.addAttribute -> https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/ui/Model.html
+            // Model.addAttribute(key, value) -> key와 value형태로 값으로 view에 전달할 수 있다.
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+
+        model.addAttribute("taco", new Taco());
+
+        return "design";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients.stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+
+}
